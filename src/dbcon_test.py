a2d506b0b45b9f6f24d57c3e3f982839781ebc47
Janos <jcr.fueting@gmail.com>
2024-11-03 21:02:55 -0500

    updated dbcon_test

--- src/dbcon_test.py
+++ src/dbcon_test.py
@@ -1,17 +1,8 @@
-from aws_advanced_python_wrapper import AwsWrapperConnection
 import psycopg
 
-awsconn = AwsWrapperConnection.connect(
-    psycopg.Connection.connect,
-    host="database-2.cluster-czkmkismw47i.us-west-2.rds.amazonaws.com",
-    dbname="datathon_db",
-    secrets_manager_secret_id="arn:aws:secretsmanager:us-west-2:668768749855:secret:rds!cluster-aa54c25e-601a-4c58-97a4-cff5aab6fb75-LYilyP",
-    secrets_manager_region="us-west-2",
-	plugins="aws_secrets_manager"
-)
+conn = psycopg.connect("postgresql://janos:(q0)r:0|QL>N[}b>>PA<6Eu6fE5x@database-3.cluster-czkmkismw47i.us-west-2.rds.amazonaws.com/datathon_db")
 
-awscursor = awsconn.cursor()
-awscursor.execute("SELECT aurora_db_instance_identifier()")
-awscursor.fetchone()
-for record in awscursor:
-    print(record)
+c = conn.cursor()
+
+print(c.execute("SELECT now()").fetchall())
+
